cbc721cda5b7b5a848b2f7f9a1190970cab891c9
emekadefirst <victorchibuogwu33@gmail.com>
2025-03-06 13:56:57 +0100

    tickets

--- src/pages/components/trending.jsx
+++ src/pages/components/trending.jsx
@@ -7,7 +7,7 @@ const TrendingComponent = () => {
       <div className="container mx-auto px-6">
         <div className="flex items-center justify-between mb-12">
           <h2 className="text-2xl md:text-3xl font-bold text-white">
-            Trending This Week
+            Trending
           </h2>
           <a
             href="/tickets"
